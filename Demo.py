5f40ad8bbd5dd66409952c5b3600e6018d55e6a2
XuejiFang <Jaggar.Fang@hotmail.com>
2022-11-07 19:15:07 +0800

    完善功能1和2

--- Demo.py
+++ Demo.py
@@ -31,6 +31,25 @@ def init(tx):
     return songs, albums, singers
 
 
+# 2.1. 查询指定日期前发行的专辑
+
+def albums_by_date_back(tx, date):
+    albums = []
+    result = tx.run("match(m:`专辑`) where m.album_release_date <= $DATE return m as album", DATE=date)
+    for record in result:
+        albums.append(record["album"])
+    return albums
+
+
+# 2.2. 查询指定日期后发行的专辑
+def albums_by_date_forward(tx, date):
+    albums = []
+    result = tx.run("match(m:`专辑`) where m.album_release_date >= $DATE return m as album", DATE=date)
+    for record in result:
+        albums.append(record["album"])
+    return albums
+
+
 # 3. 歌曲的专辑名
 def album_name_by_song(tx, song):
     names = []
@@ -105,10 +124,45 @@ def get_album_releasedBy(singer):
     return ans
 
 
+# 功能1
+def func1():
+    print('输入格式[op](op=1, 2, 3 分别表示歌曲、专辑、歌手)')
+    op = int(input())
+    if op == 1:
+        print(all_songs)
+    elif op == 2:
+        print(all_albums)
+    elif op == 3:
+        print(all_singers)
+    else:
+        print('请重新输入')
+
+
+# 功能2
+def func2():
+    print('输入格式[start,end](start和end表示起止时间，格式为Y/M)')
+    cns = input()
+    [start, end] = cns.split(',')
+
+    album_start = []
+    with driver.session() as session:
+        songs = session.read_transaction(albums_by_date_forward, start)
+        for song in songs:
+            # print(song['song_name'])
+            album_start.append(song['album_name'])
+    album_end = []
+    with driver.session() as session:
+        songs = session.read_transaction(albums_by_date_back, end)
+        for song in songs:
+            # print(song['song_name'])
+            album_end.append(song['album_name'])
+    ans = [i for i in album_start if i in album_end]
+    print(ans)
+    return ans
 # 交互
 def homepage():
     print('0. Exit\n'
-          '1. 查询歌曲、专辑、歌手\n'
+          '1. 查询所有歌曲、专辑、歌手\n'
           '2. NULL\n'
           '3. 查询歌曲在什么专辑里\n'
           '4. 查询专辑有哪些歌曲\n'
@@ -117,9 +171,9 @@ def homepage():
     if op == 0:
         return 0
     elif op == 1:
-        pass
+        func1()
     elif op == 2:
-        pass
+        func2()
     elif op == 3:
         print('请输入需要查询的歌曲：')
         q_song = input()
@@ -140,6 +194,7 @@ def homepage():
 
     return 1
 
+
 if __name__ == '__main__':
     # 连接Neo4j
     # @Hakii
@@ -149,4 +204,3 @@ if __name__ == '__main__':
     get_all()  # 获取全部信息
     while homepage():
         pass
-
